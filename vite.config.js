aea4eef263974c41afc414428a430d0ba340dfe6
Gagan <gagansasan91@yahoo.com>
2024-11-30 13:14:11 -0700

    changed name of project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build", // Change output folder to 'build'
+  },
   base: "/react-stepper/",
   plugins: [react()],
 });
